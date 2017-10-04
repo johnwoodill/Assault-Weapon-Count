808d6413ff1bf7fb20c8bb2e2b5a826ea436f911
johnwoodill <johnwoodill@gmail.com>
2017-10-04 10:08:42 -1000

    updating plot

--- figure.R
+++ figure.R
@@ -80,7 +80,7 @@ ggplot(newdat, aes(Year, Fatalities)) +
   annotate("text", x = 1999, y = 50, label = "Assault Weapons Ban") +
   geom_smooth(aes(color = "Fatalities"), se = FALSE,  size = .5) + 
   geom_smooth(aes(Year, AWeapons*10, color = "Assault Weapons"), se = FALSE,  size = .5) +
-  xlab(NULL) + ylab("Fatalaties \n (per 100 million)") +
+  xlab(NULL) + ylab("Fatalities \n (per 100 million)") +
   scale_y_continuous(sec.axis = sec_axis( ~./10, name = "Assault Weapons Count")) +
   scale_x_continuous(breaks = seq(1982, 2017, by = 5)) +    
   theme(legend.position = c(0,1), 
@@ -92,9 +92,3 @@ ggplot(newdat, aes(Year, Fatalities)) +
   scale_color_manual(values = c("#ef8a62", "#67a9cf", "#67a9cf"),
                      guide = guide_legend(override.aes = list(linetype = c(rep("blank", 3)),
                                                               shape = c(16, 1, 16))))
-
-# Reddit post text
-After the horrible tragedy in Las Vegas on Sunday night, I wanted to know if there was a trend associated with assault weapons and mass shootings. In particular, what happened after the assault weapons ban from 1994-2004?
-Data
-The data is from US Mass Shootings, 1982-2017: Data From Mother Jones’ Investigation. I counted the total number of assault rifles for each event and added as a new variable. Since we don't know excactly how many weapons were used during the recent Las Vegas event, I coded the event as three since that is what has been confirmed as assault weapons.
-R Code (downloads data and plots figure):
